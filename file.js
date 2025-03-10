a06dcfda58d6659cf96c2169abd87032a369e606
Kunal Pandey <191033820+pandey-kun7@users.noreply.github.com>
2025-03-10 15:29:04 +0530

    Basic Structure is laid

--- file.js
+++ file.js
@@ -1,50 +1,65 @@
-let humanScore = 0;
-let computerScore = 0;
-let ties=0;
-function getComputerChoice(){
-    let computerChoice= (Math.random() * 100) + 1;
-    if (computerChoice<=33){
-        return "paper";
-    }else if(computerChoice<=66 && computerChoice>33){
-        return "rock";
-    }else{
-        return "scissors";
-    }
-}
-function getHumanChoice(){
-    let humanChoice = prompt("Enter your choice punk","scissors");
-    humanChoice=humanChoice.toLowerCase()
-    return humanChoice;
-}
-let playgame=parseInt(prompt("Enter number of rounds to be played",5));
+document.addEventListener("DOMContentLoaded",()=>{
+    let humanScore = 0;
+    let computerScore = 0;
+    let ties=0;
 
-function playRound(humanChoice, computerChoice) {
-    if(humanChoice=="paper" && computerChoice=="rock"){
-        humanScore++;
-        return "Human wins!!! Paper beats rock";
-    }else if(humanChoice=="paper" && computerChoice=="scissors"){
-        computerScore++;
-        return "Computer wins!!! Scissors beats paper";
-    }else if(humanChoice=="scissors" && computerChoice=="rock"){
-        humanScore++;
-        return "Computer wins!!! Rock beats scissors";
-    }else if(humanChoice=="scissors" && computerChoice=="paper"){
-        humanScore++;   
-        return "Human wins!!! Scissors beats paper";
-    }else if(humanChoice=="rock" && computerChoice=="paper"){
-        computerScore++;
-        return "Computer wins!!! Paper beats rock"
-    }else if(humanChoice=="rock" && computerChoice=="scissors"){
-        humanScore++;
-        return "Human wins!!! Rock beats scissors";
-    }else{
-        ties++;
-    }
-  }
-  while(playgame!=0){
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    const showHumanScore=document.querySelector(".humanScore");
+    const scissors=document.querySelector(".sci");
+    const rock=document.querySelector(".rock");
+    const paper=document.querySelector(".paper");
 
-    playRound(humanSelection, computerSelection);
-    playgame--;
-}
+    scissors.addEventListener('click',()=>{
+        humanChoice="scissors";
+        playRound(humanChoice,getComputerChoice());
+    });
+    rock.addEventListener("click",()=>{
+        humanChoice="rock";
+        playRound(humanChoice,getComputerChoice());
+    });
+    paper.addEventListener("click",()=>{
+        humanChoice="paper";
+        playRound(humanChoice,getComputerChoice());
+    });
+
+    let humanChoice="";
+
+    function getComputerChoice(){
+        let computerChoice= (Math.random() * 100) + 1;
+        if (computerChoice<=33){
+            return "paper";
+        }else if(computerChoice<=66 && computerChoice>33){
+            return "rock";
+        }else{
+            return "scissors";
+        }
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        if(humanChoice=="paper" && computerChoice=="rock"){
+            humanScore++;
+            showHumanScore.textContent=humanScore;
+            console.log("Human wins!!! Paper beats rock");
+        }else if(humanChoice=="paper" && computerChoice=="scissors"){
+            computerScore++;
+            console.log("Computer wins!!! Scissors beats paper");
+        }else if(humanChoice=="scissors" && computerChoice=="rock"){
+            humanScore++;
+            showHumanScore.textContent=humanScore;
+            console.log("Computer wins!!! Rock beats scissors");
+        }else if(humanChoice=="scissors" && computerChoice=="paper"){
+            humanScore++;   
+            showHumanScore.textContent=humanScore;
+            console.log("Human wins!!! Scissors beats paper");
+        }else if(humanChoice=="rock" && computerChoice=="paper"){
+            computerScore++;
+            console.log("Computer wins!!! Paper beats rock");
+        }else if(humanChoice=="rock" && computerChoice=="scissors"){
+            humanScore++;
+            showHumanScore.textContent=humanScore;
+            console.log("Human wins!!! Rock beats scissors");
+        }else{
+            ties++;
+        }
+    } 
+});
+
